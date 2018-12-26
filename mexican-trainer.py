6829e2db7083481a9d0062ce255bfa1c9774c349
Jared Kosanovic <jared.kosanovic@wisc.edu>
2018-12-26 12:15:30 -0800

    first implementation of recursive function

--- mexican-trainer.py
+++ mexican-trainer.py
@@ -1,18 +1,22 @@
-def calculate_best_path():
-    options = []
-    for domino in hand:
-        print("domino: " + str(domino))
-        if start_number in domino:
-            print("start_number found in domino")
-            #remaining_hand = hand
-            #remaining_hand.remove(domino)
-            #print("remaining hand: " + str(remaining_hand))
-            #options.append([[domino],remaining_hand])
-            options.append(domino)
+import pprint
 
-    
-    print(options)
+def calculate_routes(route, remaining_hand):
+    last_number = route[-1][1]
+    for domino in remaining_hand:
+        if last_number in domino:
+            remaining_hand.remove(domino)
 
+            if domino[1] == last_number:
+                domino.reverse()
+
+            route.append(domino)
+            calculate_routes(route, remaining_hand)
+        else:
+            options.append(route)
+
+options = []
 start_number = 3
 hand = [[3,10],[3,4],[10,5],[5,0],[10,12],[12,11]]
-calculate_best_path()
+calculate_routes([[3,3]], hand)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(options)
