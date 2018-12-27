3a26b7b468fc6f35db480b669f0d3e9cda8f0d17
Jared Kosanovic <jared.kosanovic@wisc.edu>
2018-12-27 09:19:15 -0800

    recursive function working

--- mexican-trainer.py
+++ mexican-trainer.py
@@ -2,21 +2,23 @@ import pprint
 
 def calculate_routes(route, remaining_hand):
     last_number = route[-1][1]
-    for domino in remaining_hand:
+    route_terminated = True
+    for index, domino in enumerate(remaining_hand):
         if last_number in domino:
-            remaining_hand.remove(domino)
-
-            if domino[1] == last_number:
-                domino.reverse()
-
-            route.append(domino)
-            calculate_routes(route, remaining_hand)
-        else:
-            options.append(route)
+            route_terminated = False
+            remaining_hand_copy = remaining_hand.copy()
+            del remaining_hand_copy[index]
+            domino_copy = domino.copy()
+            if domino_copy[1] == last_number:
+                domino_copy.reverse()
+            route_copy = route.copy()
+            route_copy.append(domino_copy)
+            calculate_routes(route_copy, remaining_hand_copy)
+    if route_terminated:
+        options.append(route.copy())
 
 options = []
-start_number = 3
-hand = [[3,10],[3,4],[10,5],[5,0],[10,12],[12,11]]
+hand = [[3,12],[3,4],[3,10],[10,5],[5,0],[12,10],[12,11]]
 calculate_routes([[3,3]], hand)
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(options)
