40d72eb7c290ee6004b203f8e6e86ff031c2d6e9
Julian Kosanovic <jmkosanovic@gmail.com>
2018-12-28 10:24:29 -0800

    All error handling working correctly. Function ready to be implemented.

--- enter_hand.py
+++ enter_hand.py
@@ -2,11 +2,11 @@
 Project: Domino Trainer
 Name(s): Jared Kosanovic, Julian Kosanovic
 Date: 12/25/2018
-Version: ??
+Version: Final
 Notes: enter_hand() function for project
-Doesn't currently work
+
 """
-#
+
 def check_int(x): 
 	try:
 		int(x)
@@ -14,7 +14,6 @@ def check_int(x):
 	except ValueError:
 		return False 
 
-#
 def enter_hand():
 	domino_list = []
 	print("Please enter the two groups of numbers on a domino separated by a comma,", #Enter domino separated by comma
@@ -24,54 +23,35 @@ def enter_hand():
 		domino = input()
 		if domino == '': #Enter nothing to finish
 			break
-		#domino=[int(num) for num in domino.split(',')]
-		#
 		domino = domino.split(',')
-		print(domino) #test
 		for i in domino:
-			#print(i) #test
 			if check_int(i) == False: #If input is not an integer, error
 				print("You must enter only integers representing the dots on the domino. Try again.")
 				error_flag = True
 				break #for loop, not while loop
 			else:
 				i = int(i)
-				#print("int") #test
-				#print(i) #test
 				if i < 0: #Value <0 returns error	
 					print("Value must be greater than or equal to 0. Try again.")
 					error_flag = True
 					break
 		if error_flag == False:	
 			domino=[int(num) for num in domino]
-			print("current domino", domino) #test
-		if domino_list != None: #run if domino_list is not empty to check for duplicates
-			test = []
-			test = domino
-			test.reverse()
+		if domino_list != []: #run if domino_list is not empty to check for duplicates
 			for j in domino_list:
-				print("j", j) #test last
-				print("test", test) #test
-				#print("reverse test", test.reverse()) #test
-				if j == domino or j == test: #if duplicate
+				if j == domino: #if duplicate
 					print("That domino is already in the list. Try again.")
 					error_flag = True
+					break
+				domino.reverse() #reverse and check
+				if j == domino: #if duplicate
+					print("That domino is already in the list. Try again.")
+					error_flag = True
+					break
+				else:
+					domino.reverse() #back to normal
 		if error_flag == False: #Everything is fine, put the domino in the list
 			print(domino, "added to list")
 			domino_list.append(domino)
-		#print(domino) #test
-	print("domino list", domino_list) #test
-	#Check each domino input 
-	#Check for duplicate dominoes in the hand
-	#Return array of dominos
-	
-enter_hand()
-
-""" Code graveyard that may be useful somewhere else
-
-for i in range(0,len(domino_list)):
-		for j in range(0,2):
-			#print(domino_list[i][j])#test
-			if domino_list[i][j] < 0:
-	
-"""
+	print("Here is the final list of dominos you entered:", domino_list) #test
+	return domino_list #Return array of dominos
