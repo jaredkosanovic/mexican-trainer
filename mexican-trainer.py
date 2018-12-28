e390206de1b264142aeaa4bf3be196d65bca0623
Jared Kosanovic <jared.kosanovic@wisc.edu>
2018-12-27 16:19:03 -0800

    implement comparison logic

--- mexican-trainer.py
+++ mexican-trainer.py
@@ -25,16 +25,39 @@ def get_scored_routes():
         for domino in route:
             for number in domino:
                 score += number
-        scored_routes[score] = route
+
+        if score in scored_routes:
+            existing_route = scored_routes[score]
+            existing_route_length = len(existing_route)
+            current_route_length = len(route)
+
+            if existing_route_length < current_route_length:
+                # Favor the route with more dominos if the score is equal
+                scored_routes[score] = route
+            elif existing_route_length == current_route_length:
+                # If the amount of dominos and dots in the routes are equal,
+                # favor the route with the most doubles.
+                existing_route_doubles = count_doubles(existing_route)
+                current_route_doubles = count_doubles(route)
+                if existing_route_doubles < current_route_doubles:
+                    scored_routes[score] = route
+        else:
+            scored_routes[score] = route
 
     return scored_routes
 
+# Returns the number of dominos in a route that are doubles
+def count_doubles(route):
+    return len([domino for domino in route if domino[0] == domino[1]])
+
 routes = []
-hand = [[3,12],[3,4],[3,10],[10,5],[5,0],[12,10],[12,11]]
+hand = [[3,12],[3,4],[3,10],[10,5],[5,0],[4,10],[10,11],[12,10],[12,11],[12,12],[333,333]]
 calculate_routes([[3,3]], hand)
 print("all possible routes")
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(routes)
+
+print("number of possible routes: " + str(len(routes)))
 
 scored_routes = get_scored_routes()
 print("scored routes")
